bd1f4ebcddbf4f166ef2fdb6b2399349f9e7593a
Timothy Br <TimothyB@TSB-MBP.local>
2018-05-28 10:58:32 +0200

    Catching up

---
+++ src/extern.php
@@ -0,0 +1,11 @@
+<?php
+
+// require_once lädt die Datei nur einmal
+// durch Prüfung/check, ob Datei schon geladen wurde
+// (z.B. Funktion "test" darf nicht redefiniert werden)
+require_once 'extern_require.php'
+require_once 'extern_require.php'
+require_once 'extern_require.php'
+
+
+?>
---
+++ src/schachbrett.php
@@ -0,0 +1,38 @@
+<?php
+
+$x = 20;
+$y = 20;
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title>Schach-WM 2018</title>
+        <style>
+            td {
+                height: 50px;
+                width: 50px;
+            }
+            .black { 
+                background-color: #000000;
+            }
+            .white {
+                background-color: #ffffff;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+        <?php for ($i = 0; $i < $y; $i++): ?>
+        <tr>
+            <?php for ($j = 0; $j < $x; $j++): ?>
+                <?php if (($j + $i) % 2 == 1): ?>
+                    <td class="black"></td>
+                <?php else: ?>
+                    <td class="white"></td>
+                <?php endif; ?>
+            <?php endfor; ?>
+        </tr>
+        <?php endfor; ?>
+        </table>
+    </body>
+</html>
